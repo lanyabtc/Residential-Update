d9187659cc752ca869f0c421b95d869c7185aeac
lanyabtc <lanyabtc@gmail.com>
2026-07-20 19:38:40 +0800

    Delete app directory

--- app/api/campaigns/route.js
+++
@@ -1,6 +0,0 @@
-import { generateCampaigns } from "../../../lib/campaigns";
-
-export async function GET() {
-  const campaigns = generateCampaigns(new Date(), 2);
-  return Response.json({ campaigns });
-}
